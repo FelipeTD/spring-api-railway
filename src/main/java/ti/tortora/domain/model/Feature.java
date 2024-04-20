aaaafc5f1cacaab2cf416460a09d60eb70421570
filipe.dias <fedispato@gmail.com>
2024-04-19 21:41:51 -0300

    feat: Adicionando ORM e repository

---
+++ src/main/java/ti/tortora/domain/model/Feature.java
@@ -0,0 +1,8 @@
+package ti.tortora.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ src/main/java/ti/tortora/domain/model/News.java
@@ -0,0 +1,8 @@
+package ti.tortora.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
